4fc5a99bd8fb9e3afd10993838ebfc3267e032a5
Gerard Hynes <gkhynes@gmail.com>
2019-09-18 07:58:10 +0100

    Add CSS modules for header and layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,52 @@
 import React from "react";
 import { Link } from "gatsby";
+import headerStyles from "./header.module.scss";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Gerard Hynes</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Gerard Hynes
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className={headerStyles.navItem}
+              to="/"
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="blog">Blog</Link>
+            <Link
+              to="blog"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              to="/contact"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
